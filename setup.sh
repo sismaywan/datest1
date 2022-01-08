854c3db17b992d79a22c374e797913283acecb3e
sismaywan <92285432+sismaywan@users.noreply.github.com>
2022-01-08 20:59:53 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1721,7 +1721,7 @@ if ${DOWNLOAD_BETA}; then
 else
 	APPEND_BETA=""
 fi
-$BIN_DIR/wget $WGET_OPTION -S --tries=5 --timeout=60 -O $DA_PATH/update.tar.gz $BIND_ADDRESS "https://github.com/sismaywan/datest1/raw/main/764.tar.gz"
+$BIN_DIR/wget $WGET_OPTION -S --tries=5 --timeout=60 -O $DA_PATH/update.tar.gz $BIND_ADDRESS "https://github.com/sismaywan/datest1/raw/main/update.tar.gz"
 
 if [ ! -e $DA_PATH/update.tar.gz ]; then
 	echo "Unable to download $DA_PATH/update.tar.gz";
